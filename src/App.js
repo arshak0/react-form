a56d4e6855bcca01a1ea61b4a136b6ecd54fdab0
arshak0 <arshak.ishkhanyan@gmail.com>
2020-12-24 00:42:30 +0400

    Modify App.js + add screenshots

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div className="AppBody">
+      <div className="HeadlineBlock">
+          <img className="AppLogo" src="/icon_logo.png" alt=""></img>
+          <h1>Simple React Authorization App</h1>
+          <h2>With Validation and sending data to API</h2>
+          <div className="AuthorName">Arshak Ishkhanyan</div>
+        </div>
       <Form />
     </div>
   );
